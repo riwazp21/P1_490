3caed11561b23bdae712bb39ef3af517c5a7c130
Riwaz Poudel <rpoudel@node0.rpoudel-195893.siue-cs590-490.emulab.net>
2024-03-24 13:52:56 -0600

    Changes made with the preference of professor

--- Rawfile/NonAccess/bedtools.sh
+++ Rawfile/NonAccess/bedtools.sh
@@ -1,19 +1,23 @@
 #!/bin/bash
 
 # Path to the directory containing .bed files
-bed_dir="/users/rpoudel/490_P1/Rawfile/NonAccess"
+bed_dir=$(pwd)
 
 # Path to the reference genome fasta file
-ref_genome="/users/rpoudel/490_P1/reference/hg38.fa"
+ref_genome="../../reference/hg38.fa"
 
 # Path to the output directory
-output_dir="/users/rpoudel/490_P1/Bedtools/NonAccess/"
+output_dir="../../Bedtools/NonAccess/"
 
 # Loop through all .bed files in the directory
 for bed_file in "$bed_dir"/*.bed; do
-    # Get the base name of the file without the extension
-    base_name=$(basename -- "$bed_file" .bed)
-    
-    # Run bedtools getfasta command
-    bedtools getfasta -fi "$ref_genome" -bed "$bed_file" -fo "$output_dir/${base_name}.txt"
+    # Check if the bed_file is not empty
+    if [ -f "$bed_file" ]; then
+        # Get the base name of the file without the extension
+        base_name=$(basename -- "$bed_file" .bed)
+        
+        # Run bedtools getfasta command
+        bedtools getfasta -fi "$ref_genome" -bed "$bed_file" -fo "$output_dir/${base_name}.txt"
+    fi
 done
+
